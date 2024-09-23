fb2443bc34a1b814a445925667544960d996bb9e
Alberto Gonzalez Abril <alberto@gonzabril.es>
2024-09-23 17:36:11 +0100

    Made website scroll to the top whenever the user travels to a new page

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom';
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
+import ScrollToTop from './ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
+    <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>,
